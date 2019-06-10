741bbd24df8dd4533fec8a12bad752b219e6e0d2
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-06-10 12:21:05 +0200

    linking

--- gen.lisp
+++ gen.lisp
@@ -27,6 +27,9 @@ cl-gen-cpp-wasm
     (write-source *main-cpp-filename* "c" code)))
 ;; clang --target=wasm32 -emit-llvm -c -S wasm_01.c
 ;; llc -march=wasm32 -filetype=obj wasm_01.ll 
+;; asm-ld --no-entry --export-all -o wasm_01.wasm wasm_01.o
 ;; sudo pacman -S wabt
 ;; wasm-objdump -x wasm_01.o
+
+
 
